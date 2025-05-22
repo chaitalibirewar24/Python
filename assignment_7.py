1d2e3662e66382957f5067cc55f527009f06f7e1
chaitalibirewar24 <chaitalibirewar24@gmail.com>
2025-05-22 16:43:37 +0530

    Add files via upload

---
+++ assignment_7.py
@@ -0,0 +1,27 @@
+#que3
+'''
+rto={"pune":"MH07","chanda":"MH34"}
+def rtogenerator(x):
+    number=rto.get(x)
+    print("vehical number:",number+"EY"+"2324")
+userr=input("enter city")
+rtogenerator(userr)
+'''
+
+#que4
+'''
+doc={"ortho":("xyz",48793928),"gynac":("abc",987327)}
+def emergency(x):
+    info=doc.get(x)
+    print(info)
+userr=input("enter emergency")
+emergency(userr)
+'''
+#que6
+'''
+age=[2,2,3,4,4,5]
+def remove():
+    new=set(age)
+    print(new)
+remove()
+'''
